3cab4cfe38c3183551f68043c4685958444676fc
Rodolphe Belouin <rodolphe.belouin@gmail.com>
2018-08-16 11:45:47 +0200

    zsh: add gcloud sdk to $PATH

--- .config/zsh/variables.zsh
+++ .config/zsh/variables.zsh
@@ -9,3 +9,4 @@ export PATH=$PATH:$ANDROID_HOME/platform-tools
 export PATH="$PATH:/usr/local/opt/protobuf@2.6/bin"
 export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/latest/bin
 export PATH=$PATH:/Applications/FirefoxNightly.app/Contents/MacOS/
+export PATH=$PATH:/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin
