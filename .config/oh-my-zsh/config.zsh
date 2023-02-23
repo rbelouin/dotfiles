46f38b38abf17f70b8601008428ca0918551b571
Rodolphe Belouin <rodolphe.belouin@gmail.com>
2023-02-23 13:29:52 +0100

    zsh: try alternatives to `cat` and `diff`

--- .config/oh-my-zsh/config.zsh
+++ .config/oh-my-zsh/config.zsh
@@ -9,6 +9,8 @@ alias c='git --git-dir="$HOME/src/personal/dotfiles.git/" --work-tree="$HOME"'
 alias f='fzf'
 alias g='git'
 alias e="$EDITOR"
+alias cat='bat'
+alias diff='delta'
 
 # For some reason, ^R and ^T are not properly bound by fzf when the oh-my-zsh environment gets initialized
 bindkey '^R' fzf-history-widget
